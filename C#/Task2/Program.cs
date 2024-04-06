5e26c6726b6ee630c8f579414ce1f44ba752153b
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-06 22:02:36 +0700

    Lesson1:Task2 - 'Примеры арифметических операций'

---
+++ C#/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int i1 = 100;
+int i2 = 215;
+
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 27;
+long sum2 = sum1 + l1;
+Console.WriteLine(sum2);
+
+double d1 = 14.17137;
+double sum3 = sum2 + d1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
