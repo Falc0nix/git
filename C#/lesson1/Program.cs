cda65eade7542a695f7ed01a76edc4b014c48833
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-06 21:22:47 +0700

    add some variables

--- C#/lesson1/Program.cs
+++ C#/lesson1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
 
+//Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 17.237;
+Console.WriteLine(d);
+
+char c = '!';
+Console.WriteLine(c);
+
+bool b = true;
+Console.WriteLine(b);
