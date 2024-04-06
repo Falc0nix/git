ace82a0577f66b21e97caf7d99a9df31543f175f
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-06 23:33:19 +0700

    Задача 5. Приер работы циклов

---
+++ C#/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 1;
+int n = 100;
+
+while (i <= n)
+{
+    //Console.WriteLine(i);
+    Console.Write(i + " ");
+    i = i + 1;
+}
