e3db2c8c99ab85b1f7da035ca568759f918f2930
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-06 23:32:16 +0700

    Задача 4. Пример работы множественного оператора сравнения

---
+++ C#/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+int a = 5;
+int b = 7;
+int c = 13;
+int d = 4;
+int e = 9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
