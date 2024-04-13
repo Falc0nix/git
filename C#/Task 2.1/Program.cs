66b251d2c70249bc3d35c8619a040d4aed192056
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-13 20:32:10 +0700

    Задача 2.1. Простейшая работа с элементами массива

---
+++ C#/Task 2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5];
+arr[0] = 3;
+arr[1] = 5;
+arr[2] = 9;
+arr[3] = 1;
+arr[4] = 3;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+int[] arr3 = { 1, 2, 3, 4, 5 };
