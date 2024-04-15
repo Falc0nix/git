ff15a7fa943b7fc74d68b2d3db11faa449f453b5
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-15 23:17:24 +0700

    Задача 2.4. Генерирование случайного n-размерного массива

---
+++ C#/Task 2.4/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 10); 
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
