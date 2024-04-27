72bce5bd3675594fa0d374db8b5dcdb765e2ca20
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-27 22:03:02 +0700

    Выполнена задача 2.1 из Домашнего Задания

---
+++ C#/Homework 2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите искомое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value % 7 == 0 && value % 23 == 0)
+{
+    Console.WriteLine($"Заданное число {value} проверку. Оно кратно и 7, и 23");
+}
+else
+{
+    Console.WriteLine($"Заданное число {value} не прошло проверку");
+}
