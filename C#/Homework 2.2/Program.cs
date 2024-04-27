2fb742bb0f71958fb6a69aade53948ae0477a544
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-27 22:30:00 +0700

    Выполнена задача 2.2 из Домашнего Задания

---
+++ C#/Homework 2.2/Program.cs
@@ -0,0 +1,50 @@
+﻿
+int x = 0;
+int y = 0;
+
+while (x == 0)
+{
+  System.Console.Write("Введите координату x: "); 
+  x = Convert.ToInt32(Console.ReadLine());
+  
+  if (x == 0)
+  {
+    continue;
+  }
+  else
+  {
+  break;
+  } 
+}
+
+while (y == 0)
+{
+  System.Console.Write("Введите координату y: "); 
+  y = Convert.ToInt32(Console.ReadLine());
+  
+  if (y == 0)
+  {
+    continue;
+  }
+  else
+  {
+  break;
+  } 
+}
+
+if (x > 0 && y > 0)
+  {
+    Console.WriteLine($"Заданное точка с координатами ({x}, {y}) находится в 1 четверти координатной плоскости");
+  }
+else if (x < 0 && y > 0)
+  {
+    Console.WriteLine($"Заданное точка с координатами ({x}, {y}) находится в 2 четверти координатной плоскости");
+  }
+  else if (x < 0 && y < 0)
+  {
+    Console.WriteLine($"Заданное точка с координатами ({x}, {y}) находится в 3 четверти координатной плоскости");
+  }
+    else if (x > 0 && y < 0)
+  {
+    Console.WriteLine($"Заданное точка с координатами ({x}, {y}) находится в 4 четверти координатной плоскости");
+  }
