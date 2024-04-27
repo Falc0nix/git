342bea79e725cc08ee9144951affa27b27fe627b
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-28 01:05:57 +0700

    Выполнена задача 2.3 из Домашнего Задания

---
+++ C#/Homework 2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿int value = 0;
+
+int lowerlimit = 10;
+int upperlimit = 99;
+int maximum = 0;
+
+
+while (value == 0) 
+{
+  System.Console.Write($"Введите целое число из диапазона ({lowerlimit}, {upperlimit}) : "); 
+  value = Convert.ToInt32(Console.ReadLine());
+  
+  if ((value > upperlimit) ^ (value < lowerlimit))
+  {
+    value = 0;
+    continue;
+  }
+
+}
+
+
+int value_digit = value % 10;
+int value_decimal = value / 10;
+
+if (value_digit >= value_decimal)
+{
+    maximum = value_digit;
+}
+else
+{
+    maximum = value_decimal;
+}
+
+Console.WriteLine($"В полученном числе {value} максимальная цифра - {maximum}");
