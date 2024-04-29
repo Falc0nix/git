d21ebd5448d1e93a575ec9956161e2ae741839ab
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-30 01:40:22 +0700

    Выполнена Задача 4.3 из Домашнего Задания

---
+++ C#/Homework 4.3/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] myarray = new int[size];
+
+Random rand = new Random();
+
+for (int i = 0; i < myarray.Length; i++)
+{
+    myarray[i] = rand.Next(1, 100); 
+}
+
+Console.WriteLine("Исходный массив: ");
+
+for (int i = 0; i < myarray.Length; i++)
+{
+    Console.Write(myarray[i] + " ");
+}    
+Array.Reverse(myarray);
+
+Console.WriteLine("");
+Console.WriteLine("Обратный массив: ");
+
+for (int i = 0; i < myarray.Length; i++)
+{
+    Console.Write(myarray[i] + " ");
+}    ;
