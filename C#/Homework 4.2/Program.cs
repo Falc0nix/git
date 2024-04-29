3e2a101f819cf1c7f67d7b53b58207f6418cabf1
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-30 01:23:02 +0700

    Выполнена задача 4.2 из Домашнего Задания

---
+++ C#/Homework 4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int[] array = new int[size];
+
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 999); 
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+    if (array[i] % 2  == 0)
+    {
+      count++;
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Количество четных чисел в массиве: " + count);
