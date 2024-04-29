6cba04e9441aa24ecfd12a6736b3a69d88f066c2
Falc0nix <a.soloviev.85@inbox.ru>
2024-04-30 01:27:41 +0700

    Выполнена Задача 1.1 из Домашнего Задания

---
+++ C#/Homework 1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите первое число: ");
+ашкыетшConsole.Write("Введите первое число: ");
+int a = 5;
+int b = 7;
+int c = 13;
+int d = 4;
+int e = 9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+
+Console.WriteLine("Hello, World!");
