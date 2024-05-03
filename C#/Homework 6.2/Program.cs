6a92074517ccf54f2eccdeddc65e30804ba2bb6f
Falc0nix <a.soloviev.85@inbox.ru>
2024-05-03 23:34:36 +0700

    Выполнена Задача 6.2 из Домашнего задания

---
+++ C#/Homework 6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+    string query  = "";
+    string result  = "";
+    Console.Write ("Задайте строку, содержащую латинские буквы в обоих регистрах: ");
+    
+    query = Console.ReadLine();
+    
+    while (result == "")
+    {
+        if (!System.Text.RegularExpressions.Regex.IsMatch(query, (@"[A-Z]")))
+        {
+            
+            Console.Write ("Задайте строку, содержащую ТОЛЬКО латинские буквы в обоих регистрах: ");
+            query = Console.ReadLine();
+            continue;
+        }
+        else
+        {
+        result = query.ToLower();
+        }
+    }
+    
+
+Console.WriteLine (result);
