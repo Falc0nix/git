d5f544a27a46180881b5763c916827de362e2ee3
Falc0nix <a.soloviev.85@inbox.ru>
2024-05-03 23:58:09 +0700

    Выполнена Задача 6.3 из Домашнего задания

---
+++ C#/Homework 6.3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+    string query  = "";
+    string result  = "";
+    Console.Write ("Задайте строку произвольную текстовую строку (давайте проверим, будет ли она являться палиндромом: ");
+    
+    query = Console.ReadLine();
+    
+    while (result == "")
+    {
+        if (query == "")
+        {
+            
+            Console.Write ("Текстовая строка не введена. Задайте строку произвольную текстовую строку: ");
+            query = Console.ReadLine();
+            continue;
+        }
+        else
+        {
+
+            char[] stringArray = query.ToCharArray();
+            Array.Reverse(stringArray);
+            result = new string(stringArray);
+        }
+    }
+    
+
+if (query == result)
+{
+     Console.WriteLine ($"Заданная строка {query} является палиндромом");
+}
+else
+{
+    Console.WriteLine ($"Заданная строка {query} НЕ является палиндромом");
+}
+
