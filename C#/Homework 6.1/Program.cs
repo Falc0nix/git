ddcc282e24af2cbbbf4ff763cbaf6344a04f858d
Falc0nix <a.soloviev.85@inbox.ru>
2024-05-03 22:05:19 +0700

    Выполнена задача 6.1 из Домашнего Задания

---
+++ C#/Homework 6.1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write("Задайте количество строк двумерного массива: ");
+int rowCount = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте количество столбцов двумерного массива: ");
+int columnCount = Convert.ToInt32(Console.ReadLine());
+
+
+char[,] matrix = new char[rowCount, columnCount];
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"Задайте элемент [{i}, {j}] символьного массива: ");
+        matrix[i, j] = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("");
+    }
+}
+
+string result ="";
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        result = result + matrix[i, j];
+
+    }
+}
+
+Console.WriteLine (result);
