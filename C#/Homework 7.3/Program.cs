a4285eafce120d8ec074710cb198910a345e2438
Falc0nix <a.soloviev.85@inbox.ru>
2024-05-04 23:49:31 +0700

    Выполнена Задача 7.3 из Домашнего Задания

---
+++ C#/Homework 7.3/Program.cs
@@ -0,0 +1,16 @@
+﻿    void print_array(int [] array_temp, int count) 
+    { 
+      if (count > 0) 
+      { 
+      	Console.Write(array_temp[count] + " "); 
+      	print_array(array_temp, count - 1); 
+      } 
+      else Console.WriteLine(array_temp[0]); 
+    } 
+
+    /////////////
+    ///
+
+      int [] array = {2, 8, 16, 32, 64, 128}; 
+      print_array(array, array.Length - 1); 
+
