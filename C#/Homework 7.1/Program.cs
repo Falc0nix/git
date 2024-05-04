f92b719f10955822ff89f6d048612c5dec3aa1f9
Falc0nix <a.soloviev.85@inbox.ru>
2024-05-04 22:38:09 +0700

    Выволнена Задача 7.1 Домашнего Задания

---
+++ C#/Homework 7.1/Program.cs
@@ -0,0 +1,36 @@
+﻿int count(int mini, int maxi)
+{
+    if (mini == maxi)
+    {
+            return mini;
+    }
+    Console.Write(mini + ", ");
+    return count(mini + 1, maxi);
+}
+
+//////////////////
+int m = 0;
+int n = 0;
+
+  checkpoint_1:
+  System.Console.Write("Введите натуральное число m: "); 
+  m = Convert.ToInt32(Console.ReadLine());
+  
+  if (m <= 0)
+  {
+    goto checkpoint_1;
+  }
+
+  checkpoint_2:
+  System.Console.Write("Введите натуральное число n: "); 
+  n = Convert.ToInt32(Console.ReadLine());
+  
+  if (n <= 0)
+  {
+    goto checkpoint_2;
+  }
+
+int min_value = Math.Min(m, n);
+int max_value = Math.Max(m, n);
+
+Console.Write(count(min_value, max_value ));
